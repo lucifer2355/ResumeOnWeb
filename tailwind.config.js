c9fbecc07eeecff6b5de4a3b8e698c5f643d0b05
Dhruvil Gajjar <dgajjar999@gmail.com>
2023-01-05 23:14:32 +0530

    feat: config color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      black: "#020203",
+      white: "#F3F3F4",
+      gray: "#A6A9AA",
+      "green-100": "#02F74C",
+      "green-200": "#1D5D33",
+      "green-300": "#089729",
+      "green-400": "#19C84A",
+      "green-500": "#76A988",
+    },
   },
   plugins: [],
 };
