fb78cfb6920eed7a4867d2c905dcc51b2a3b884a
Dhruvil Gajjar <dgajjar999@gmail.com>
2023-01-21 22:56:25 +0530

    feat: add header style

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { headers } from "../../data/headers";
+import { cn } from "../../utils/cn";
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <div className={cn("grid grid-cols-2 px-8 pt-4")}>
+      {headers.map(({ title }, index) => (
+        <span key={title} className={cn("text-gray-1 even:text-end")}>
+          {`//0${index + 1}. <${title}>`}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default Header;
